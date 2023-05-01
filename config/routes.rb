9a20703f6e94bef4ca1c94a6ac14957c28161471
Natalia Cuenca <ncuenca@chicagobooth.edu>
2023-05-01 17:35:04 +0000

    Started photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-get("/users",{:controller => "users", :action => "index"})
+  get("/",{:controller => "users", :action => "index"})
+
+  get("/users",{:controller => "users", :action => "index"})
 
 get("/users/:path_username",{:controller => "users", :action => "show"})
+
+get("/photos",{:controller => "photos", :action => "index"})
+
+get("/photos:path_username",{:controller => "photos", :action => "show"})
 end
